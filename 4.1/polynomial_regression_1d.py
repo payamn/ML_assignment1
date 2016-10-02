c3ba76b251f22864142bf9cf1951361b5b48106c
Payam <payam222@gmail.com>
2016-10-02 12:27:57 -0700

    4.2.1 finish

--- 4.1/polynomial_regression_1d.py
+++ 4.1/polynomial_regression_1d.py
@@ -10,47 +10,43 @@ from numpy.linalg import inv
 def main():
     # Get input data
     (countries, features, values) = a1.load_unicef_data()
-
+    print features
     targets = values[:, 1]
-    x = values[:, 7:]
+    #x = values[:, 7:]
 
     # normalize data
     # x = a1.normalize_data(x)
 
     # set param value
-    PlotMatrixRMSErorr, PlotMatrixRMSErorrTest = calculateToPlot(x, targets)
+    PlotMatrixRMSErorr =[]
+    PlotMatrixRMSErorrTest = []
+    for i in range (7 , 15):
+        x = values[:,i]
+        rmsErrorTrain, rmsErorrTest = calculateToPlotPolynomial3(x, targets)
+        PlotMatrixRMSErorr.append(rmsErrorTrain[0,0])
+        PlotMatrixRMSErorrTest.append(rmsErorrTest[0,0])
     # Set EW
     # Ew = t_train - (0.5*np.square(PhiTrain))*w
-    plotMatrixDegree = np.matrix([[1], [2], [3], [4], [5], [6]])
+    plotMatrixDegree = np.matrix([[1], [2], [3], [4], [5], [6],[7],[8]])
+    labels = features [7:15]
+    plt.xticks([1,2,3,4,5,6,7,8],labels,rotation='vertical')
     plt.figure(1)
-    plt.subplot(211)
-    plt.plot(plotMatrixDegree, PlotMatrixRMSErorr)
+    plt.subplot(111)
+    plt.tight_layout()
+    #plt.plot(plotMatrixDegree, PlotMatrixRMSErorr)
+    plt.bar(plotMatrixDegree-0.2, PlotMatrixRMSErorr , width=0.1, color="red")
     plt.ylabel('RMS')
-    plt.legend(['Test error', 'Training error'])
-    plt.title('Fit with polynomials, no regularization')
-    plt.xlabel('Polynomial degree')
-    plt.subplot(211)
-    plt.plot(plotMatrixDegree, PlotMatrixRMSErorrTest)
-    plt.ylabel('RMS')
-    plt.legend(['Test error', 'Training error'])
-    plt.title('Fit with polynomials, no regularization')
-    plt.xlabel('Polynomial degree')
+    plt.legend([ 'Training error', 'Test error'])
+    plt.title('Feature 8 to 16')
+    #plt.xlabel('Polynomial degree')
 
-    x = a1.normalize_data(x)
-    PlotMatrixRMSErorr, PlotMatrixRMSErorrTest = calculateToPlot(x, targets)
-
-    plt.subplot(212)
-    plt.plot(plotMatrixDegree, PlotMatrixRMSErorr)
+    plt.subplot(111)
+    # plt.plot(plotMatrixDegree, PlotMatrixRMSErorr)
+    plt.bar(plotMatrixDegree, PlotMatrixRMSErorrTest, width=0.1, color="Blue")
     plt.ylabel('RMS')
-    plt.legend(['Test error', 'Training error'])
-    plt.title('Fit with polynomials, no regularization and normalize')
-    plt.xlabel('Polynomial degree')
-    plt.subplot(212)
-    plt.plot(plotMatrixDegree, PlotMatrixRMSErorrTest)
-    plt.ylabel('RMS')
-    plt.legend(['Test error', 'Training error'])
-    plt.xlabel('Polynomial degree')
-
+    plt.legend(['Training error', 'Test error'])
+    plt.title('Feature 8 to 16')
+    #plt.xlabel('Polynomial degree')
     plt.show()
 
 
@@ -60,6 +56,7 @@ def RMS(matrixA, matrixB):
     sumTemp = sum(temp)
     sumTemp = sumTemp / 100
     sumTemp = np.sqrt(sumTemp)
+
     return sumTemp
 
 
@@ -74,25 +71,23 @@ def calculateThetaTrainPolynomial(X, N, polynomialDegree):
     return ThetaTrain
 
 
-def calculateToPlot(x, targets):
+def calculateToPlotPolynomial3(x, targets):
     N_TRAIN = 100;
     x_train = x[0:N_TRAIN, :]
     x_test = x[N_TRAIN:, :]
     t_train = targets[0:N_TRAIN]
     t_test = targets[N_TRAIN:]
 
-    PlotMatrixRMSErorr = np.zeros((6, 1))
-    PlotMatrixRMSErorrTest = np.zeros((6, 1))
-    for i in range(1, 7):
-        PhiTrain = calculateThetaTrainPolynomial(x_train, N_TRAIN, i)
-        PhiTest = calculateThetaTrainPolynomial(x_test, 95, i)
-        # Set W
-        sInverce = np.linalg.pinv(PhiTrain)
-        w = sInverce * t_train
-        ourTrainValidation = PhiTrain * w
-        ourTestValidation = PhiTest * w
-        PlotMatrixRMSErorr[i - 1] = RMS(t_train, ourTrainValidation)
-        PlotMatrixRMSErorrTest[i - 1] = RMS(t_test, ourTestValidation)
+    i = 3
+    PhiTrain = calculateThetaTrainPolynomial(x_train, N_TRAIN, i)
+    PhiTest = calculateThetaTrainPolynomial(x_test, 95, i)
+    # Set W
+    sInverce = np.linalg.pinv(PhiTrain)
+    w = sInverce * t_train
+    ourTrainValidation = PhiTrain * w
+    ourTestValidation = PhiTest * w
+    PlotMatrixRMSErorr = RMS(t_train, ourTrainValidation)
+    PlotMatrixRMSErorrTest = RMS(t_test, ourTestValidation)
     return PlotMatrixRMSErorr, PlotMatrixRMSErorrTest
 
 
